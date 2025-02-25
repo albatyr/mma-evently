de61c2d3f0f2a606c7310386bbaf0f28fcb471db
Nursultan Albatyrov <nursultan@promomash.com>
2025-02-25 12:45:37 +0500

    Add Keycloak identity

--- src/API/Evently.Api/Program.cs
+++ src/API/Evently.Api/Program.cs
@@ -37,7 +37,8 @@ builder.Services.AddInfrastructure(
 
 builder.Services.AddHealthChecks()
     .AddNpgSql(databaseConnectionString)
-    .AddRedis(redisConnectionString);
+    .AddRedis(redisConnectionString)
+    .AddUrlGroup(new Uri(builder.Configuration.GetValue<string>("KeyCloak:HealthUrl")!), HttpMethod.Get, "keycloak");
 
 builder.Configuration.AddModuleConfiguration(["events", "users", "ticketing"]);
 builder.Services.AddEventsModule(builder.Configuration);
@@ -66,5 +67,9 @@ app.MapHealthChecks("health", new HealthCheckOptions
 app.UseSerilogRequestLogging();
 
 app.UseExceptionHandler();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 await app.RunAsync();
